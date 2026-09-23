27dc6ec8dcf3f72330898ce7c9193d72de32ec1b
JasonLiu <jasonliudehui227@gmail.com>
2026-09-23 10:59:00 +0100

    First lab setup with coordinates implemented

---
+++ app/src/main/java/com/example/placemark_lab2/models/PlacemarkModel.kt
@@ -0,0 +1,10 @@
+package com.example.placemark_lab2.models
+
+data class PlacemarkModel (
+    var id: Long = 0L,
+    var title: String = "",
+    var description: String = "",
+    var x: Double = 0.0,
+    var y: Double = 0.0
+
+)
